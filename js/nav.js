8fccd6437b980a53f4f90bc6a400db4313fa1c91
Jesús Otero <jesusoterogomez@gmail.com>
2015-04-12 16:36:04 -0430

    Improved fixed header positioning

--- js/nav.js
+++ js/nav.js
@@ -1,15 +1,18 @@
 jQuery(document).ready(function ($) {
-  
+
  var wpOffset = 0;
  var navOffset = $('.headernav').offset().top;
-   $(window).scroll(function (event) {
-
-    wpOffset = $('#wpadminbar').height();
-    
+ $(window).scroll(function (event) {
+    //get admin bar height
+    if( $('body').hasClass('admin-bar')){
+      wpOffset = $('#wpadminbar').height();
+    }
     if($(this).scrollTop() >= navOffset - wpOffset) {
       $('.headernav').addClass('fixed');
+      $('body').css('padding-top',$('.headernav').height() + 'px');
     } else {
       $('.headernav').removeClass('fixed');
+      $('body').css('padding-top',0);
     }
 
   });
